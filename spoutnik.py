d8beaf707bb440aca33365172288c78a1a5682ec
Mathias <mathias.te@googlemail.com>
2014-02-07 06:27:47 +0100

    Works fine, needs to be enhanced.

--- spoutnik.py
+++ spoutnik.py
@@ -7,36 +7,83 @@
 import netCDF4
 from netCDF4 import Dataset
 
-file_url = '../Spoutnik-I files/PREVIMER_WW3-FINIS-200M_20140123T13Z.nc'
 
-# Load the file:
-ncfile = Dataset(file_url,'r')
+class Spoutnik(object):
 
-point_indexes = dict({'lat':0,'lon':0})
-point = dict({'lat':48.29793167114258,'lon':-4.976805686950684})
+    ncfile = None
+    point = dict({'lat': 48.29793167114258, 'lon': -4.976805686950684})
+    point_indexes = dict({'lat': 0, 'lon': 0})
+    var_list = \
+        list(['hs', 'hs0', 'hs1', 'dir', 'th0', 'th1', 'tp0', 'tp1', 'tp2'])
+    var_values = list()
 
-def index(pt, ndarray):
-    ndarray_cp = ndarray.copy()
-    dist_sq = (ndarray_cp - pt)**2
-    index = dist_sq.argmin()
-    return index
+    def __init__(self, file_url=
+        '../Spoutnik-I files/PREVIMER_WW3-FINIS-200M_20140123T13Z.nc'):
+        """
+        Load the netcdf file
+        """
+        self.ncfile = Dataset(file_url, 'r')
+        pass
 
-point_indexes['lat'] = index(point['lat'], ncfile.variables[u'latitude'][:])
-point_indexes['lon'] = index(point['lon'], ncfile.variables[u'longitude'][:])
-point_indexes
+    def __refresh_index(self, pt, ndarray):
+        """
+        Retrieve from the numpy.ndarray the index of a given value of the array
+        """
+        ndarray_cp = ndarray.copy()
+        dist_sq = (ndarray_cp - pt) ** 2
+        index = dist_sq.argmin()
+        return index
 
-times = ncfile.variables['time']
-jd = netCDF4.num2date(times[:],times.units)
+    def refresh_indexes(self):
+        # Latitude
+        self.point_indexes['lat'] = self.__refresh_index(
+            self.point['lat'], self.ncfile.variables[u'latitude'][:])
+        # Longitude
+        self.point_indexes['lon'] = self.__refresh_index(
+            self.point['lon'], self.ncfile.variables[u'longitude'][:])
+        pass
 
-# for that point, retrieve the values for the variable list:
-var_list = list(['hs','hs0','hs1','dir','th0','th1','tp0','tp1','tp2'])
+    def refresh_var_values(self):
+        del(self.var_values[:])
+        for i in self.var_list:
+            var_value = dict({'name': "", 'value': 0, 'units': ""})
+            var = self.ncfile.variables[i]
+            var_value['name'] = i
+            var_value['units'] = var.units
+            var_value['value'] = \
+                var[0, self.point_indexes['lon'], self.point_indexes['lat']]
+            self.var_values.append(var_value)
+        pass
 
-print jd
+    def print_attr(self):
 
-print "point: ", point
+        # format the date:
+        times = self.ncfile.variables['time']
+        jd = netCDF4.num2date(times[:], times.units)
 
-for i in var_list:
-    var = ncfile.variables[i]
-    print i, "(", var.units ,"): ", var[0, point_indexes['lon'], point_indexes['lat']]
+        # print Dimensions:
+        print "date: ", jd
+        print "point: ", self.point
+        print "point_indexes: ", self.point_indexes
+        print ""
+
+        # print Variables:
+        for i in self.var_values:
+            print ("  " +
+                i['name'] +
+                ":\t" +
+                str(i['value']) +
+                "\t" +
+                i['units']
+                )
+        pass
+
+    pass
+
+if __name__ == "__main__":
+    sp = Spoutnik()
+    sp.refresh_indexes()
+    sp.refresh_var_values()
+    sp.print_attr()
 
 
