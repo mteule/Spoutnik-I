4ae6cd5864b43ddd19bf3b8154311c7f63928347
Mathias <mathias.te@googlemail.com>
2014-02-08 04:21:55 +0100

    Beginning the excell.py file

---
+++ spoutnik/excell.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 Mathias Teulé <mathias.te@googlemail.com>
+
+# Usefull documentation to create excell files can be found at the
+# official homepage:
+# http://xlsxwriter.readthedocs.org/tutorial01.html
+
+import xlsxwriter
