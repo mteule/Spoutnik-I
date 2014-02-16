fc39ce866fc028577b662b2cb65d8e94fd9558a2
Mathias <mathias.te@googlemail.com>
2014-02-16 08:14:23 +0100

    Add spoutnik.Launcher()

--- spoutnik/spoutnik.py
+++ spoutnik/spoutnik.py
@@ -9,7 +9,8 @@ from netCDF4 import Dataset
 
 
 class Spoutnik(object):
-
+    """
+    """
     ncfile_url = None
     ncfile = None
     point = dict({'lat': 48.29793167114258, 'lon': -4.976805686950684})
@@ -36,20 +37,20 @@ class Spoutnik(object):
         self.dim_values[0:2] = ""
         dim = {}
         # date
-        dim['name'] = u'date'
+        dim['name'] = 'date'
         dim['units'] = ""
         times = self.ncfile.variables['time']
         jd = netCDF4.num2date(times[:], times.units)
         dim['value'] = jd
         self.dim_values.append(dim.copy())  # append() do not copy
         # u'latitude'
-        dim['name'] = u'latitude'
+        dim['name'] = 'latitude'
         dim['units'] = self.ncfile.variables['latitude'].units
         dim['value'] = self.ncfile.variables[
                 'latitude'][self.point_indexes['lat']]
         self.dim_values.append(dim.copy())
         # u'longitude'
-        dim['name'] = u'longitude'
+        dim['name'] = 'longitude'
         dim['units'] = self.ncfile.variables['longitude'].units
         dim['value'] = self.ncfile.variables[
                 'longitude'][self.point_indexes['lon']]
@@ -78,10 +79,10 @@ class Spoutnik(object):
         """
         # Latitude
         self.point_indexes['lat'] = self.__refresh_index(
-            self.point['lat'], self.ncfile.variables[u'latitude'][:])
+            self.point['lat'], self.ncfile.variables['latitude'][:])
         # Longitude
         self.point_indexes['lon'] = self.__refresh_index(
-            self.point['lon'], self.ncfile.variables[u'longitude'][:])
+            self.point['lon'], self.ncfile.variables['longitude'][:])
         pass
 
     def refresh_var_values(self):
@@ -106,7 +107,7 @@ class Spoutnik(object):
         times = self.ncfile.variables['time']
         jd = netCDF4.num2date(times[:], times.units)
 
-        print "\n======= selected variable's values for a GPS point ======="
+        print ("\n======= selected variable's values for a GPS point =======")
 
         # print File data:
         print "ncfile_url: ", self.ncfile_url
@@ -131,10 +132,70 @@ class Spoutnik(object):
 
     pass
 
+
+class Launcher(object):
+    """
+    Launches various Spoutnik() to retrieve 'spots_var_values_list'[]
+    from a 'file_url' netcdf file for a given 'spots_list'[]
+    """
+    file_url = ""
+    spots_list = []
+    spots_var_values_list = []
+    sp = None  # Spoutnik()
+
+    def __init__(self, spots_list=None, file_url=None):
+        self.spots_list = spots_list
+        self.file_url = file_url
+        self.sp = Spoutnik(self.file_url)
+        pass
+
+    def _get_one(self, point={}, position=0):
+        self.sp.point = point
+        self.sp.refresh()
+        var_position = {'name': 'position', 'value': 0, 'units': None}
+        var_position['value'] = position
+        vals = []
+        vals.append(var_position)
+        vals = vals + self.sp.values
+        self.spots_var_values_list.append(vals)
+
+    def _get_all(self):
+        del(self.spots_var_values_list[:])
+        self.sp.file_url = self.file_url
+        for i in range(len(self.spots_list)):
+            spot = self.spots_list[i]
+            self._get_one(spot['spot'], spot['position'])
+        self.sp.ncfile.close()
+    pass
+
 if __name__ == "__main__":
-    sp = Spoutnik()
+    # Testing Spoutnik()
+    sp = Spoutnik(
+        '../../Spoutnik-I files/PREVIMER_WW3-FINIS-200M_20140123T13Z.nc')
     sp.refresh()
     sp.print_attr()
+
+    spots_list = [
+        {'position':1, 'spot':
+            {'lat': 48.29793167114258, 'lon': -4.976805686950684}},
+        {'position':2, 'spot':
+            {'lat': 48.176185607910156, 'lon': -4.864180088043213}},
+        {'position':3, 'spot':
+            {'lat': 48.177852630615234, 'lon': -4.606392860412598}}
+        ]
+
+    # Testing Launcher()
+    lau = Launcher(
+        spots_list=spots_list,
+        file_url=
+            '../../Spoutnik-I files/PREVIMER_WW3-FINIS-200M_20140123T13Z.nc')
+    lau._get_all()
+
+    print lau.spots_var_values_list
+    for i in lau.spots_var_values_list:
+        print "\nNew spot:"
+        for var in i:
+            print var
 
 
 
